5f0d2e551525051d483b9783f2054644cd1c0f56
Collins Gitonga <collinsgitonga296@gmail.com>
2025-04-26 08:44:35 +0300

    Added AI chatbot integration

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,3 +89,8 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(card);
   });
 });
+
+
+//chatbase ai chatbot
+
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="sm4Blc9mNxIxiPDfxqXD9";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
